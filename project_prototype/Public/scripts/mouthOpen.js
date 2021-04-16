fbb422b223a7b2bcc928cd2fdf1fc72ca2651a7f
shirinstar <shirinanlen@gmail.com>
2021-04-15 19:00:24 -0700

    gradually speed

--- project_prototype/Public/scripts/mouthOpen.js
+++ project_prototype/Public/scripts/mouthOpen.js
@@ -1,21 +1,11 @@
 // @input Component.MeshVisual meshVisual;
-// @input Asset.Material[] materials;
-// input SceneObject long_root;
+// @input Asset.Material materials;
 
-//script.long_root.enabled = false;
 var timeStampsOpen = []; 
 var timeOpen = 0;
 var oldTime = 0;
-
+var isTaking = false;
 var currentItemIndex = 0;
-// Function to clear the current materials and add a new one
-function setMaterial(material){
- script.meshVisual.clearMaterials();
- script.meshVisual.addMaterial(material);
-}
-
-setMaterial(script.materials[currentItemIndex]);
-
   
 function onMouthOpened() {
     timeOpen = getTime();
@@ -23,9 +13,10 @@ function onMouthOpened() {
     
     timeStampsLongArray =  timeStampsOpen.filter(function (time) { return time > (timeOpen-10)});
     print("fast" + timeStampsLongArray)
-    if (currentItemIndex == 1 && timeStampsLongArray.length >= 3 ) {
-        currentItemIndex = 0;
-        setMaterial(script.materials[currentItemIndex]);
+    
+    if (timeStampsLongArray.length >= 3 ) {
+        isTaking = true;
+        print(script.materials.mainPass.matTime)
     }  
 }
 
@@ -36,18 +27,24 @@ mouthOpenedEvent.bind(onMouthOpened);
 function isStopSpeaking() {
     timeStampsShortArray =  timeStampsOpen.filter(function (time) { return time > (getTime()-5)});
     print("slow" + timeStampsShortArray)   
-    if (currentItemIndex == 0  && timeStampsShortArray.length <= 1) {
-        currentItemIndex = 1;
-        setMaterial(script.materials[currentItemIndex]);
+    if (timeStampsShortArray.length <= 1) {
+        isTaking = false;
         timeStampsOpen = []
     }  
  }
+
+var speed = script.materials.mainPass.matTime
 
 function onUpdate (time) {
   newTime = getTime()
   if (newTime - oldTime > 1) {
         isStopSpeaking();
         oldTime = newTime
+    }
+     if(isTaking && speed > 0) {
+     script.materials.mainPass.matTime -=0.0003
+    } else if (!isTaking && speed < 0.5) {
+        script.materials.mainPass.matTime +=0.0003
     }
 }
 
