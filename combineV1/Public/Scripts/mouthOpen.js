b85c72efe2741b7d127e9fb5960151a3c3c71128
shirinstar <shirinanlen@gmail.com>
2021-04-21 17:25:21 -0700

    wip timings

--- combineV1/Public/Scripts/mouthOpen.js
+++ combineV1/Public/Scripts/mouthOpen.js
@@ -1,5 +1,6 @@
-// @input Component.MeshVisual[] meshVisualPetals;
-// @input Asset.Material materials;
+//@input Component.MeshVisual[] meshVisualPetals;
+//@input Asset.Material petalsMaterials;
+//@input Asset.Material rootsMaterials;
 //@input SceneObject[] avkans;
 
 var timeStampsOpen = []; 
@@ -8,55 +9,72 @@ var oldTime = 0;
 var isTalking = false;
 var currentItemIndex = 0;
 var countAvkan = 0;
+var countPetals = 0;
 var updateAvkan = false;
 var avkanState = null;
-var longWait = 10;
-var mediumWait = 5;
-var shortWait = 2;
+var startWait = 30; //seconds
+var longWait = 8; //seconds
+var mediumWait = 5; //seconds
+var shortWait = 1; //seconds
 var hasStarted = false;
 
-var speed = script.materials.mainPass.matTime;
+var speed = script.petalsMaterials.mainPass.matTime;
+var rootSpeed = script.rootsMaterials.mainPass.rootTime; 
+
+//face parts
+var rightEyeFinish = false;
+var startingBackground = false;
+  
+//turnning off//
+//inisitally false until called 
+for (var i=0; i< script.avkans.length; i++) {
+    script.avkans[i].enabled = false;
+}
+//general app delay
+var delayedStart = script.createEvent("DelayedCallbackEvent");
+delayedStart.bind(function(){
+    hasStarted = true;
+})
+delayedStart.reset(startWait)
+
+//finish face part
+//var delayedFinishFacePart = script.createEvent("DelayedCallbackEvent");
+//delayedFinishFacePart.bind(function(){
+//     startingBackground = true;
+//})
+//
+//function startBackground() {
+//    if(rightEyeFinish) {
+//       delayedFinishFacePart.reset(mediumWait)
+//    }            
+//}
   
 //general MOUTH OPEN//
 global.onMouthOpened = function() {
     timeOpen = getTime();
     timeStampsOpen.push(timeOpen);  
     
-    timeStampsLongArray =  timeStampsOpen.filter(function (time) { return time > (timeOpen-10)});
-    print("fast" + timeStampsLongArray)
+    timeStampsLongArray =  timeStampsOpen.filter(function (time) { return time > (timeOpen-5)});
+    print("calculating talking" + timeStampsLongArray)
     
     if (timeStampsLongArray.length >= 3 ) {
         isTalking = true;
-        print(script.materials.mainPass.matTime);
     }  
 }
 
 var mouthOpenedEvent = script.createEvent("MouthOpenedEvent");
 mouthOpenedEvent.bind(onMouthOpened);
 
-  
 function isStopSpeaking() {
-    timeStampsShortArray =  timeStampsOpen.filter(function (time) { return time > (getTime()-5)});
-    print("slow" + timeStampsShortArray)   
+    timeStampsShortArray =  timeStampsOpen.filter(function (time) { return time > (getTime()-7)});
+    print("calculating not talking" + timeStampsShortArray)   
     if (timeStampsShortArray.length <= 1) {
         isTalking = false;
         timeStampsOpen = []
     }  
  }
 
-//general delay//
-var delayedStart = script.createEvent("DelayedCallbackEvent");
-delayedStart.bind(function(){
-    hasStarted = true;
-})
-//when the avkan initially starts. 
-delayedStart.reset(longWait)
-
-//inisitally false until called 
-for (var i=0; i< script.avkans.length; i++) {
-    script.avkans[i].enabled = false;
-}
-
+//AVKAN//
 //delayed AVKAN//
 var delayedAvkan = script.createEvent("DelayedCallbackEvent");
 delayedAvkan.bind(function(){
@@ -67,15 +85,14 @@ function updateAvkanState() {
     if(updateAvkan===false) {
        updateAvkan = true;
         avkanState = isTalking;
-       delayedAvkan.reset(shortWait)
+       delayedAvkan.reset(mediumWait)
     }            
 }
 
 function showAvkan() {
-    print('showing')
     if(countAvkan < script.avkans.length) {
         script.avkans[countAvkan].enabled = !avkanState;
-        delayedAvkan.reset(mediumWait)
+        delayedAvkan.reset(shortWait)
         countAvkan++;
     } else {
         countAvkan = 0;
@@ -88,16 +105,27 @@ function onUpdate (time) {
   newTime = getTime()
   if (newTime - oldTime > 1) {
         isStopSpeaking();
+         if (!hasStarted) {
+        script.petalsMaterials.mainPass.matTime=0;
+    }
+        if (rightEyeFinish == false) {
+            script.rootsMaterials.mainPass.rootTime=0;
+        } 
         oldTime = newTime
     }
-     if (isTalking && speed > 0) {
-     script.materials.mainPass.matTime -=0.0003
+    
+     if (isTalking && (speed > 0)) {
+        script.petalsMaterials.mainPass.matTime -= 0.0003
         updateAvkanState();
-      
-    } else if (!isTalking && speed < 0.5) {
-        script.materials.mainPass.matTime +=0.0003;
-        if(hasStarted) {
-        updateAvkanState();
+        if(rootSpeed > 0){
+        script.rootsMaterials.mainPass.rootTime -= 0.00008;
+        }  
+    } else if (hasStarted && !isTalking && (speed < 0.6)) {
+        script.petalsMaterials.mainPass.matTime += 0.0002;
+            updateAvkanState();
+            rightEyeFinish =true;
+        if (rightEyeFinish && rootSpeed < 0.4){
+            script.rootsMaterials.mainPass.rootTime += 0.00005;
         }
     }
 }
