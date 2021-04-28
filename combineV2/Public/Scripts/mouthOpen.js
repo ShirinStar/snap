ae88694c366fe838ad1c6282741ceb7fb46d8650
shirinstar <shirinanlen@gmail.com>
2021-04-28 10:24:49 -0700

    refine speech

--- combineV2/Public/Scripts/mouthOpen.js
+++ combineV2/Public/Scripts/mouthOpen.js
@@ -1,30 +1,25 @@
-//@input SceneObject[] growingObjects;
 //@input Asset.Material rightEyePetalsMaterials;
-//@input Asset.Material rightCheekPetalMaterials;
 //@input Asset.Material rootsMaterials;
 //@input SceneObject[] avkans;
 //@input Component.MeshVisual[] meshVisualPetals;
-
 
 var timeStampsOpen = []; 
 var timeOpen = 0;
 var oldTime = 0;
 var isTalking = false;
-var currentItemIndex = 0;
+
 var countAvkan = 0;
-var countPetals = 0;
 var updateAvkan = false;
 var avkanState = null;
+
 var startWait = 5; //seconds
 var longWait = 8; //seconds
 var mediumWait = 5; //seconds
 var shortWait = 0.5; //seconds
-var testTime = 2;
-var hasStarted = false;
 
-var rightEyeSpeed = script.rightEyePetalsMaterials.mainPass.matTime;
-var rightCheakSpeed = script.rightCheekPetalMaterials.mainPass.matTime;
-var rootSpeed = script.rootsMaterials.mainPass.rootTime; 
+var hasStarted = false;
+var hasUpdatedNextStep = false;
+var hasUpdatedPrevStep = false;
 
 var currentStep = 0;
 
@@ -45,11 +40,9 @@ var growingSteps = [
     }, 
 ]
 
-//when starting everything is off
+//when starting - everything is off
 script.rightEyePetalsMaterials.mainPass.matTime=0;
-script.rightCheekPetalMaterials.mainPass.matTime=0;
 script.rootsMaterials.mainPass.matTime=0;
-
 
 for (var i=0; i< script.avkans.length; i++) {
     script.avkans[i].enabled = false;
@@ -61,7 +54,7 @@ delayedStart.bind(function(){
     hasStarted = true;
 })
 
-delayedStart.reset(startWait)
+delayedStart.reset(startWait);
 
   
 //general MOUTH OPEN//
@@ -69,9 +62,8 @@ global.onMouthOpened = function() {
     timeOpen = getTime();
     timeStampsOpen.push(timeOpen);  
     
-    timeStampsLongArray =  timeStampsOpen.filter(function (time) { return time > (timeOpen-15)});
-    print("calculating talking" + timeStampsLongArray)
-
+    timeStampsLongArray =  timeStampsOpen.filter(function (time) { return time > (timeOpen-20)});
+    print("calculating talking" + timeStampsLongArray);
     if (timeStampsLongArray.length >= 2 ) {
         isTalking = true;
     }  
@@ -81,132 +73,133 @@ var mouthOpenedEvent = script.createEvent("MouthOpenedEvent");
 mouthOpenedEvent.bind(onMouthOpened);
 
 function isStopSpeaking() {
-    timeStampsShortArray =  timeStampsOpen.filter(function (time) { return time > (getTime()-10)});
-    print("calculating not talking" + timeStampsShortArray)   
-    if (timeStampsShortArray.length <= 1) {
+    timeStampsShortArray =  timeStampsOpen.filter(function (time) { return time > (getTime()-15)});
+    print("calculating not talking" + timeStampsShortArray) ;  
+    if (timeStampsShortArray.length == 0) {
         isTalking = false;
-        timeStampsOpen = []
+        timeStampsOpen = [];
     }  
  }
 
-//AVKAN//
-//delayed AVKAN//
+
+//AVKAN// showing one by one
 var delayedAvkan = script.createEvent("DelayedCallbackEvent");
 delayedAvkan.bind(function(){
-    showAvkan()
+    showAvkan();
 })
 
 function updateAvkanState() {
     if(updateAvkan===false) {
        updateAvkan = true;
         avkanState = isTalking;
-       delayedAvkan.reset(mediumWait)
+       delayedAvkan.reset(mediumWait);
     }            
 }
 
 function showAvkan() {
     if(countAvkan < script.avkans.length) {
         script.avkans[countAvkan].enabled = !avkanState;
-        delayedAvkan.reset(shortWait)
+        delayedAvkan.reset(shortWait);
         countAvkan++;
     } else {
         countAvkan = 0;
         updateAvkan =false;
     }
 }
-var hasUpdatedNextStep = false;
-var hasUpdatedPrevStep = false;
+///END AVKAN
 
+
+//setting time of delay to increase + validating update next step
 var delayedNextStep = script.createEvent("DelayedCallbackEvent");
 delayedNextStep.bind(function(){
-     print('from Next' + currentStep)
-    hasUpdatedNextStep =false
+    hasUpdatedNextStep =false;
     if(currentStep <= growingSteps.length-2) {
-        print('inside Next IF')
-        currentStep++
+        currentStep++;
     } 
 })
 
+//setting time of delay to decrease + validating update prev step
 var delayedPrevStep = script.createEvent("DelayedCallbackEvent");
 delayedPrevStep.bind(function(){
-    hasUpdatedPrevStep =false
-    print('from Prev' + currentStep)
+    hasUpdatedPrevStep =false;
     if(currentStep > 0 ) {
-        currentStep--
+        currentStep--;
     } 
 })
 
+//checking status growing
 function isDoneGrowing(currentMaterial) {
     if(currentMaterial.objName==='rightEyePetalsMaterials') {
        return script[currentMaterial.objName].mainPass.matTime > currentMaterial.maxTime;
     } else if(currentMaterial.objName==='rootsMaterials') {
         return script[currentMaterial.objName].mainPass.matTime > currentMaterial.maxTime;
     } else if(currentMaterial.objName==='avkans') {
-        return countAvkan===script.avkans.length -1
+        return countAvkan===script.avkans.length -1;
     }
 }
 
+//what needs to happen with every material increase
 function takeStep(currentMaterial) {
     if(currentMaterial.objName==='rightEyePetalsMaterials') { 
-      return script[currentMaterial.objName].mainPass.matTime += 0.0004;
+      return script[currentMaterial.objName].mainPass.matTime += 0.0002;
     } else if (currentMaterial.objName==='rootsMaterials') {
-       return script[currentMaterial.objName].mainPass.matTime += 0.0004;
+       return script[currentMaterial.objName].mainPass.matTime += 0.0001;
     } else if (currentMaterial.objName ==='avkans') {
-        updateAvkanState()
+        updateAvkanState();
     }
 }
 
+//checking status srinking
 function isDoneShrinking(currentMaterial) {
     if(currentMaterial.objName==='rightEyePetalsMaterials') {
        return script[currentMaterial.objName].mainPass.matTime < 0;
     } else if(currentMaterial.objName==='rootsMaterials') {
         return script[currentMaterial.objName].mainPass.matTime < 0;
     } else if(currentMaterial.objName==='avkans') {
-        return countAvkan===script.avkans.length -1
+        return countAvkan===script.avkans.length -1;
     }
 }
 
+//what needs to happen with every material decrease
 function reverseStep(currentMaterial) {
     if(currentMaterial.objName==='rightEyePetalsMaterials') { 
-      return script[currentMaterial.objName].mainPass.matTime -= 0.0009;
+      return script[currentMaterial.objName].mainPass.matTime -= 0.0006;
     } else if (currentMaterial.objName==='rootsMaterials') {
-       return script[currentMaterial.objName].mainPass.matTime -= 0.0009;
+       return script[currentMaterial.objName].mainPass.matTime -= 0.0003;
     } else if (currentMaterial.objName ==='avkans') {
-        updateAvkanState()
+        updateAvkanState();
     }
 }
 
 
 //every frame///
 function onUpdate (time) {
-  newTime = getTime()
+  newTime = getTime();
   if (newTime - oldTime > 1) {
         isStopSpeaking();
-        oldTime = newTime
+        oldTime = newTime;
     }
      var currentMaterial = growingSteps[currentStep]
      if (hasStarted && isTalking) {
        
        if(isDoneShrinking(currentMaterial)) {
             if(!hasUpdatedPrevStep) {
-                hasUpdatedPrevStep = true
-                delayedPrevStep.reset(testTime)
-            }
-       } else {
-            reverseStep(currentMaterial)
-        } 
-
-        } else if (hasStarted && !isTalking) {
-         if (isDoneGrowing(currentMaterial)){
-            if(!hasUpdatedNextStep) {
-                print('updating next step')
-                hasUpdatedNextStep =true
-                delayedNextStep.reset(testTime)
+                hasUpdatedPrevStep = true;
+                delayedPrevStep.reset(mediumWait);
             }
         } else {
-            takeStep(currentMaterial)
+            reverseStep(currentMaterial);
+        } 
+    } else if (hasStarted && !isTalking) { 
+        if (isDoneGrowing(currentMaterial)) {
+            if(!hasUpdatedNextStep) {
+                print('updating next step')
+                hasUpdatedNextStep =true;
+                delayedNextStep.reset(mediumWait);
+            }
+        } else {
+            takeStep(currentMaterial);
         }
-       
     }
 }
 
